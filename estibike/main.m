ac0b212bbafa73c678c5ea160fc20672dc0d8959
John Cieslik-Bridgen <john@estimote.com>
2014-09-29 08:43:52 +0200

    Initial Commit

---
+++ estibike/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  estibike
+//
+//  Created by John Cieslik-Bridgen on 29/09/14.
+//  Copyright (c) 2014 jcb1973. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
